a2030665ea309d3dec37c314d5e975b8b213d897
ateljayant <ateljayant@gmail.com>
2022-08-17 16:42:30 +0530

    PANT

---
+++ mergetheobject.js
@@ -0,0 +1,4 @@
+function Check(obj1,obj2) {
+    let obj3 = Object.assign(obj1,obj2);
+    return obj3;
+  }
---
+++ objecthasparameterornot.js
@@ -0,0 +1,9 @@
+function Check(obj1) {
+    let a = Object.keys(obj1);
+    if (a.length == 0){
+      return false;
+    }
+    else {
+      return true;
+    }
+ }
